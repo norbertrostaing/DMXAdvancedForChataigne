64911c70b0b2e183e0138ab4006190f3bfbc3b16
norbertrostaing <7236092+norbertrostaing@users.noreply.github.com>
2021-10-04 23:23:38 +0200

    added patch helper

--- DMXAdvanced.js
+++ DMXAdvanced.js
@@ -1,94 +1,6 @@
+/// DMX processing
 
-
-/* ********** GENERAL SCRIPTING **********************
-
-		This templates shows what you can do in this is module script
-		All the code outside functions will be executed each time this script is loaded, meaning at file load, when hitting the "reload" button or when saving this file
-*/
-
-
-// You can add custom parameters to use in your script here, they will be replaced each time this script is saved
-// var myFloatParam = script.addFloatParameter("My Float Param","Description of my float param",.1,0,1); 		//This will add a float number parameter (slider), default value of 0.1, with a range between 0 and 1
-
-//Here are all the type of parameters you can create
-/*
-var myTrigger = script.addTrigger("My Trigger", "Trigger description"); 									//This will add a trigger (button)
-var myBoolParam = script.addBoolParameter("My Bool Param","Description of my bool param",false); 			//This will add a boolean parameter (toggle), defaut unchecked
-var myFloatParam = script.addFloatParameter("My Float Param","Description of my float param",.1,0,1); 		//This will add a float number parameter (slider), default value of 0.1, with a range between 0 and 1
-var myIntParam = script.addIntParameter("My Int Param","Description of my int param",2,0,10); 				//This will add an integer number parameter (stepper), default value of 2, with a range between 0 and 10
-var myStringParam = script.addStringParameter("My String Param","Description of my string param", "cool");	//This will add a string parameter (text field), default value is "cool"
-var myColorParam = script.addColorParameter("My Color Param","Description of my color param",0xff0000ff); 	//This will add a color parameter (color picker), default value of opaque blue (ARGB)
-var myP2DParam = script.addPoint2DParameter("My P2D Param","Description of my p2d param"); 					//This will add a point 2d parameter
-var myP3DParam = script.addPoint3DParameter("My P3D Param","Description of my p3d param"); 					//This will add a point 3d parameter
-var myTargetParam = script.addTargetParameter("My Target Param","Description of my target param"); 			//This will add a target parameter (to reference another parameter)
-var myEnumParam = script.addEnumParameter("My Enum Param","Description of my enum param",					//This will add a enum parameter (dropdown with options)
-											"Option 1", 1,													//Each pair of values after the first 2 arguments define an option and its linked data
-											"Option 2", 5,												    //First argument of an option is the label (string)
-											"Option 3", "banana"											//Second argument is the value, it can be whatever you want
-											); 	
-*/
-
-
-//you can also declare custom internal variable
-//var myValue = 5;
-
-/*
- The init() function will allow you to init everything you want after the script has been checked and loaded
- WARNING it also means that if you change values of your parameters by hand and set their values inside the init() function, they will be reset to this value each time the script is reloaded !
-*/
-function init()
-{
-	//myFloatParam.set(5); //The .set() function set the parameter to this value.
-	//myColorParam.set([1,.5,1,1]);	//for a color parameter, you need to pass an array with 3 (RGB) or 4 (RGBA) values.
-	//myP2DParam.set([1.5,-5]); // for a Point2D parameter, you need to pass 2 values (XY)
-	//myP3DParam.set([1.5,2,-3]); // for a Point3D parameter, you need to pass 3 values (XYZ)
-}
-
-/*
- This function will be called each time a parameter of your script has changed
-*/
-function scriptParameterChanged(param)
-{
-	//You can use the script.log() function to show an information inside the logger panel. To be able to actuallt see it in the logger panel, you will have to turn on "Log" on this script.
-	script.log("Parameter changed : "+param.name); //All parameters have "name" property
-	if(param.is(myTrigger)) script.log("Trigger !"); //You can check if two variables are the reference to the same parameter or object with the method .is()
-	else if(param.is(myEnumParam)) script.log("Key = "+param.getKey()+", data = "+param.get()); //The enum parameter has a special function getKey() to get the key associated to the option. .get() will give you the data associated
-	else script.log("Value is "+param.get()); //All parameters have a get() method that will return their value
-}
-
-/*
- This function, if you declare it, will launch a timer at 50hz, calling this method on each tick
-*/
-/*
-function update(deltaTime)
-{
-	script.log("Update : "+util.getTime()+", delta = "+deltaTime); //deltaTime is the time between now and last update() call, util.getTime() will give you a timestamp relative to either the launch time of the software, or the start of the computer.
-}
-*/
-
-
-
-function moduleParameterChanged(param)
-{
-	if(param.isParameter())
-	{
-		script.log("Module parameter changed : "+param.name+" > "+param.get());
-	}else 
-	{
-		script.log("Module parameter triggered : "+param.name);	
-	}
-}
-
-function moduleValueChanged(value)
-{
-	if(value.isParameter())
-	{
-		script.log("Module value changed : "+value.name+" > "+value.get());	
-	}else 
-	{
-		script.log("Module value triggered : "+value.name);	
-	}
-}
+var clearAllSlotsBtn = script.addTrigger("Clear all slots", "Clear all slots for all channels");
 
 var slotsData = [];
 for (var i = 0; i < 512; i++) {
@@ -150,9 +62,11 @@ function clearChannel(chanNumber, mode) {
 	}
 }
 
-
-
-
+function clearAllChannel(mode) {
+	for (var i = 0; i < 512; i++) {
+		clearChannel(i+1, mode);
+	}
+}
 
 
 function updateSlot(chanNumber, mode, slotNumber, value, dataType) {
@@ -273,7 +187,7 @@ function processRGBChannel(chanNumber) {
 		for (var i = 0; i< slotsId.length; i++) {
 			var s = data.FX[slotsId[i]];
 			for (var j = 0; j < 4; j++) {
-				val += s.value[j] * s.level;
+				val[j] += s.value[j] * s.level;
 			}
 		}
 	}
@@ -312,7 +226,6 @@ function write8bitRGB(chanNumber, value) {
 }
 
 function write16bitRGB(chanNumber, value) {
-	script.log(value[0], value[1], value[2]);
 	var r = value[0] * ((256 * 256)-1);
 	local.send(chanNumber+0, Math.floor(r/256));
 	local.send(chanNumber+1, Math.floor(r%256));
@@ -325,6 +238,54 @@ function write16bitRGB(chanNumber, value) {
 }
 
 
+////////  Patch helper
+
+var variablesGroup = script.addTargetParameter("Multiplex filler custom variable","Select the custom variable group that you wanna use as values");
+variablesGroup.setAttribute("targetType","container");
+variablesGroup.setAttribute("root",root.customVariables);
+variablesGroup.setAttribute("searchLevel",0);
+
+var inputList = script.addTargetParameter("Multiplex filler list","Select the multiplex list that you wanna fill");
+inputList.setAttribute("targetType","container");
+inputList.setAttribute("root",root.states);
+inputList.setAttribute("searchLevel",4);
+
+var fillInputListBtn = script.addTrigger("Fill input list", "Fill selected list with selected custom variable list");
+
+function fillInputList() {
+	var group = variablesGroup.getTarget();
+	var list = inputList.getTarget();
+	if (!group.variables || !list) {return;}
+
+	var variables = group.variables.getItems();
+	var inputs = getInputListElements(list);
+	var max = Math.min(variables.length, inputs.length);
+	for (var i = 0; i < max; i++) {
+		inputs[i].set(variables[i].getChild(variables[i].name));
+	}
+}
+
+function scriptParameterChanged(param) {
+	if(param.is(fillInputListBtn)) {
+		fillInputList();
+	} else if(param.is(clearAllSlotsBtn)) {
+		clearAllChannel();
+	}  
+}
+
+
+
+
+function getInputListElements(element) {
+	var children = [];
+	var props = util.getObjectProperties(element);
+	for (var i = 0; i< props.length; i++) {
+		if (element[props[i]]._type == "Controllable" && props[i].substring(0,1) == "#") {
+			children.push(element[props[i]]);
+		}
+	}
+	return children;
+}
 
 
 
@@ -332,8 +293,7 @@ function write16bitRGB(chanNumber, value) {
 
 
 
-
-
+/////// Util lib
 
 function explode(v) {
 	script.log(" proprietes : ");
@@ -350,5 +310,8 @@ function explode(v) {
 }
 
 function map(value, low1, high1, low2, high2) {
+	script.log("called with "+value+", "+low1+", "+high1+", "+low2+", "+high2);
+	script.log(high2[0]);
+
     return low2 + (high2 - low2) * (value - low1) / (high1 - low1);
 }
